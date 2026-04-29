a254288050a693d125297f6bf052f47b4fe86a8a
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-04-29 23:07:26 +0530

    Add files via upload

---
+++ digits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class digits {
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         if(sc.hasNextInt()){
+         int N=sc.nextInt();
+         for(int i=0;i<=N;i++){
+             int num=sc.nextInt();
+
+
+         }
+         }
+
+
+
+         }
+
+
+
+
+}
