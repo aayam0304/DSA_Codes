97675e8a1944d0e62fe0f701c55ed4c693da31f4
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-05-02 23:03:35 +0530

    Add files via upload

---
+++ sumofconsecutiveoddnumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class sumofconsecutiveoddnumbers {
+   public  static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int T=sc.nextInt();  //number of test cases
+       for(int t=0;t<T;t++){
+           int X=sc.nextInt();
+           int Y=sc.nextInt();
+           int start=Math.min(Y,X)+1;  //excluding X
+           int end=Math.max(Y,X)-1;    //excluding Y
+           int sum=0;
+           for(int i=start;i<=end;i++){
+               if(i%2!=0){
+                   sum+=i;
+               }
+
+           }
+           System.out.println(sum);
+
+       }
+
+    }
+}
