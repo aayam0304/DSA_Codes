65762a8e91c7c79cbe31734ee87c5b21f18d24f6
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-05-16 11:45:52 +0530

    Add files via upload

--- shape2.java
+++ shape2.java
@@ -6,10 +6,10 @@ public class shape2 {
          int N=sc.nextInt();
          for(int i=1;i<=N;i++){
              for(int j=1;j<=N-i;j++){
-                 System.out.println("");
+                 System.out.print("");
              }
              for(int k=1;k<=2*i-1;k++){
-                 System.out.println("*");
+                 System.out.print("*");
              }
              System.out.println();
          }
